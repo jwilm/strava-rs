72eb3945fd4101d18337d8bd761a81866060453e
Joe Wilm <jdwilm@gmail.com>
2015-03-31 09:20:06 -0700

    Clean up lib.rs
    Nothing here is really used anyway.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,15 @@
+#![feature(convert)]
+
+//! Strava API
+//!
+
 extern crate hyper;
 extern crate time;
-
-use std::io::Read;
+// extern crate curl;
 
 // use hyper::Client;
 // use hyper::header::Connection;
 // use hyper::header::ConnectionOption;
 
 mod http;
-mod resources;
-
-// #[test]
-// fn it_works() {
-//     let mut client = Client::new();
-//     let mut res = client.get("http://www.google.com/")
-//         .header(Connection(vec![ConnectionOption::Close]))
-//         .send().unwrap();
-// 
-//     let mut body = String::new();
-//     res.read_to_string(&mut body).unwrap();
-//     println!("Response: {}", body);
-// }
+pub mod resources;
