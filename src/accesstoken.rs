6549e2bc370f0ea49feda33dabcfa8df6fbead3c
Joe Wilm <jdwilm@gmail.com>
2015-06-05 08:17:26 -0700

    Relax lifetime requirements for AccessToken

--- src/accesstoken.rs
+++ src/accesstoken.rs
@@ -1,17 +1,33 @@
+use std::env;
 use std::convert::From;
 
 pub struct AccessToken {
-    token: &'static str
+    token: String
 }
 
-impl From<&'static str> for AccessToken {
-    fn from(s: &'static str) -> AccessToken {
-        AccessToken { token: s }
+impl AccessToken {
+    pub fn new(token: String) -> AccessToken {
+        AccessToken {
+            token: token
+        }
+    }
+
+    pub fn new_from_env() -> Result<AccessToken, env::VarError> {
+        match env::var("STRAVA_ACCESS_TOKEN") {
+            Ok(token) => Ok(AccessToken::new(token)),
+            Err(e) => Err(e)
+        }
+    }
+}
+
+impl<'a> From<&'a str> for AccessToken {
+    fn from(s: &'a str) -> AccessToken {
+        AccessToken { token: s.to_string() }
     }
 }
 
 impl AccessToken {
-    pub fn get(&self) -> &'static str {
-        self.token
+    pub fn get(&self) -> &str {
+        &self.token[..]
     }
 }
