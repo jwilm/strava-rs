f170b89525a7f1259ba0e416b29ba100058b27ac
Joe Wilm <jdwilm@gmail.com>
2015-06-06 08:20:46 -0700

    Add ability to get stats for athlete

--- src/resources/athlete.rs
+++ src/resources/athlete.rs
@@ -14,7 +14,6 @@ use error::ApiError;
 /// The object is returned in detailed, summary or meta representations.
 ///
 /// See: http://strava.github.io/api/v3/athlete/
-#[allow(dead_code)]
 #[derive(RustcDecodable, Debug)]
 pub struct Athlete {
     pub id: i32,
@@ -46,6 +45,44 @@ pub struct Athlete {
     // pub bikes: Vec<Gear>
 }
 
+/// Statistics for an athlete
+///
+/// This data is only retrieveable for the currently authenticated athlete. Values are in seconds
+/// and meters.
+///
+/// http://strava.github.io/api/v3/athlete/#stats
+#[derive(Debug, RustcDecodable)]
+pub struct Stats {
+    biggest_ride_distance: f32,
+    biggest_climb_elevation_gain: f32,
+    recent_ride_totals: RecentTotals,
+    recent_run_totals: RecentTotals,
+    ytd_ride_totals: Totals,
+    ytd_run_totals: Totals,
+    all_ride_totals: Totals,
+    all_run_totals: Totals,
+}
+
+#[derive(Debug, RustcDecodable)]
+pub struct RecentTotals {
+    count: i32,
+    distance: f32,
+    moving_time: i32,
+    elapsed_time: i32,
+    elevation_gain: f32,
+    achievement_count: i32
+}
+
+#[derive(Debug, RustcDecodable)]
+pub struct Totals {
+    count: i32,
+    distance: i32,
+    moving_time: i32,
+    elapsed_time: i32,
+    elevation_gain: i32
+}
+
+
 
 impl Athlete {
     pub fn get_current(token: &AccessToken) -> Result<Athlete, ApiError> {
@@ -56,6 +93,12 @@ impl Athlete {
     pub fn get(token: &AccessToken, id: i32) -> Result<Athlete, ApiError> {
         let url = format!("https://strava.com/api/v3/athletes/{}?access_token={}", id, token.get());
         http::get::<Athlete>(&url[..])
+    }
+
+    pub fn stats(&self, token: &AccessToken) -> Result<Stats, ApiError> {
+        let url = format!("https://strava.com/api/v3/athletes/{}/stats?access_token={}",
+                          self.id, token.get());
+        http::get::<Stats>(&url[..])
     }
 }
 
@@ -69,7 +112,7 @@ mod api_tests {
     #[test]
     fn get_current_athlete() {
         let token = AccessToken::new_from_env().unwrap();
-        let athlete: Athlete = Athlete::get_current(&token).unwrap();
+        let athlete = Athlete::get_current(&token).unwrap();
         assert_eq!(athlete.resource_state, ResourceState::Detailed);
     }
 
@@ -80,5 +123,15 @@ mod api_tests {
         let athlete = Athlete::get(&token, id).unwrap();
         println!("{:?}", athlete);
         assert_eq!(athlete.id, id);
+    }
+
+    #[test]
+    #[allow(dead_code)]
+    fn get_athlete_stats() {
+        let token = AccessToken::new_from_env().unwrap();
+        let athlete = Athlete::get_current(&token).unwrap();
+        let stats = athlete.stats(&token).unwrap();
+
+        println!("{:?}", stats);
     }
 }
