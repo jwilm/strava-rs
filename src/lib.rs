db3ffc1342672728e8cc8b815b65200dffc5e944
Joe Wilm <jdwilm@gmail.com>
2015-06-08 08:33:56 -0700

    Add a mod comment in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
-//! Strava API
+//! A wrapper for the Strava API
 //!
+//! # Organization
+//!
+//! The module layout in this crate mirrors the [Strava API documentation][]. All functions
+//! interacting with the strava servers will return a
+//! [`strava::error::Result`](error/type.Result.html) for which the `E` parameter is curried to
+//! [`strava::error::ApiError`](error/enum.ApiError.html).
+//!
+//! [Strava API Documentation]: http://strava.github.io/api/
 
 extern crate hyper;
 extern crate rustc_serialize;
